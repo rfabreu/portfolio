c3f1d6207c59f74ef4a1b602f53a26828ac5dd11
rfabreu <raabreugomes@gmail.com>
2022-08-11 01:30:11 -0400

    Updated projects to render online images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,7 +20,8 @@ function Projects() {
                         <a
                             href={project.link}
                             key={project.image}
-                            className="sm:w-1/2 w-100 p-4">
+                            className="sm:w-1/2 w-100 p-4"
+                            target="_blank" rel="noreferrer">
                             <div className="flex relative">
                                 <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src={project.image} />
                                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
