732f5c12431a1882784b6d2ba4928dd7819e7461
rfabreu <raabreugomes@gmail.com>
2022-08-10 23:09:02 -0400

    Built the Projects component

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        link: "https://github.com/rfabreu",
+    },
+    {
+        title: "React Tracks",
+        subtitle: "React and Python",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.git",
+        link: "https://github.com/rfabreu",
+    },
+    {
+        title: "DevChat",
+        subtitle: "React and Firebase",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-3.gif",
+        link: "https://github.com/rfabreu",
+    },
+    {
+        title: "Epic Test App",
+        subtitle: "React Hooks",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://github.com/rfabreu",
+    },
+];
