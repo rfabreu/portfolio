263d9a5e352b1a94ff353b16bd817da6bc1648a4
rfabreu <raabreugomes@gmail.com>
2022-08-10 23:39:33 -0400

    Built the Testimonials component

--- src/data.js
+++ src/data.js
@@ -3,30 +3,51 @@ export const projects = [
         title: "React Reserve",
         subtitle: "MERN Stack",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-1.gif",
+        image: "../public/logo192.png",
         link: "https://github.com/rfabreu",
     },
     {
         title: "React Tracks",
         subtitle: "React and Python",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-2.git",
+        image: "../public/logo192.png",
         link: "https://github.com/rfabreu",
     },
     {
         title: "DevChat",
         subtitle: "React and Firebase",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-3.gif",
+        image: "../public/logo192.png",
         link: "https://github.com/rfabreu",
     },
     {
         title: "Epic Test App",
         subtitle: "React Hooks",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-4.gif",
+        image: "../public/logo192.png",
         link: "https://github.com/rfabreu",
     },
 ];
 
 export const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'MySQL', 'Python', 'Kubernetes', 'Object-Oriented Programming'];
+
+export const testimonials = [
+    {
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "../public/logo192.png",
+        name: "First_Name Last_Name",
+        company: "Some Company",
+    },
+    {
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "../public/logo192.png",
+        name: "First_Name Last_Name",
+        company: "Some Company",
+    },
+    {
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "../public/logo192.png",
+        name: "First_Name Last_Name",
+        company: "Some Company",
+    },
+];
