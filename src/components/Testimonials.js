85d2bdd8f10732a53daed22d80a01db0c4884967
rfabreu <raabreugomes@gmail.com>
2022-08-10 22:50:44 -0400

    Created initial file structure

---
+++ src/components/Testimonials.js
@@ -0,0 +1,3 @@
+function Testimonials(){}
+
+export default Testimonials;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/App.js", "./src/components/About.js", "./src/components/Contact.js", "./src/components/Navbar.js", "./src/components/Projects.js", "./src/components/Skills.js", "./src/components/Testimonials.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
