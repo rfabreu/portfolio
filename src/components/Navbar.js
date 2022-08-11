425a2edb1fe57c70db5742914fff96237ed19d0c
rfabreu <raabreugomes@gmail.com>
2022-08-11 02:31:34 -0400

    Disabled Testimonials nav button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,9 +18,9 @@ function Navbar() {
                     <a href="#skills" className="mr-5 hover:text-white">
                         Skills
                     </a>
-                    <a href="#testimonials" className="mr-5 hover:text-white">
+                    {/* <a href="#testimonials" className="mr-5 hover:text-white">
                         Testimonials
-                    </a>
+                    </a> */}
                 </nav>
                 <a href="#contact" className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
                     Contact
