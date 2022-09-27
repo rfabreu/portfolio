2e7aca4b98a440b51539025cdfe9a10550e0ad31
rfabreu <raabreugomes@gmail.com>
2022-09-27 01:33:59 -0400

    Added smooth scroll to About buttons

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import hero from "../images/hero.png";
+import { Link } from "react-scroll";
 
 function About() {
     return (
@@ -15,12 +16,12 @@ function About() {
                     I studied at the University of Toronto coding Bootcamp, and recently acquired a certificate from Red Had in OpenShift and Kubernetes. Throughout the years I've demonstrated success in the technology, television, and education industries. Where through teamwork and leadership I contributed to implementing innovative solutions. My goal is to help you build great things through good Software Engineering practices.
                     </p>
                     <div className="flex justify-center">
-                        <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                        <Link to="contact" activeClass="active" spy={true} smooth={true} offset={-70} duration={1000} style={{cursor:"pointer"}} className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
                             Work With Me
-                        </a>
-                        <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white ronded text-lg">
+                        </Link>
+                        <Link to="projects" activeClass="active" spy={true} smooth={true} offset={-70} duration={1000} style={{cursor:"pointer"}} className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white ronded text-lg">
                             See My Work
-                        </a>
+                        </Link>
                     </div>
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
