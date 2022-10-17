86ee91a5d7f7d0abf4e05637988989601d13cdf1
rfabreu <raabreugomes@gmail.com>
2022-10-17 16:24:04 -0400

    Updated About section content v0.1

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,14 @@ function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Hi, I'm Rafael.
+                        Hi,
+                        <br className="hidden lg:inline-block" />
+                        I'm Rafael,
                         <br className="hidden lg:inline-block" />
                         <span className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Software Engineer and Full-stack Web Developer.</span>
                     </h1>
                     <p className="mb-8 leading-relaxed">
-                    I studied at the University of Toronto coding Bootcamp, and recently acquired a certificate from Red Had in OpenShift and Kubernetes. Throughout the years I've demonstrated success in the technology, television, and education industries. Where through teamwork and leadership I contributed to implementing innovative solutions. My goal is to help you build great things through good Software Engineering practices.
+                    I studied at the <strong>University of Toronto</strong> coding Bootcamp, and received a certificate from Red Had in <strong>Kubernetes</strong> through their <strong>OpenShift</strong> platform Bootcamp. Throughout the years I've demonstrated success in the Technology, Television, and Education industries. Where through teamwork and leadership I contributed to implementing innovative solutions. My goal is to effectively help you build great things through good <strong>Software Engineering</strong> practices.
                     </p>
                     <div className="flex justify-center">
                         <Link to="contact" activeClass="active" spy={true} smooth={true} offset={-70} duration={2000} style={{cursor:"pointer"}} className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
